01d62748a3a7b1c872c7beee4ffe424c70eb51ff
Beto Beckerman Cie <94578320+betobeckermancie@users.noreply.github.com>
2024-11-01 14:12:39 -0600

    Update Analisis_Citas.py

--- Analisis_Citas.py
+++ Analisis_Citas.py
@@ -1,1 +1,4 @@
 #Enfocado para hacer analisis por fechas representandose por fecha
+
+# Cargar el archivo CSV
+df = pd.read_csv("/dbfs/mnt/processed/Ads_General_Por_Dia/anuncios_insights_general_por_dia_limpiado.csv")
